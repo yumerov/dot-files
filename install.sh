076f859500a17c617fad090e59f4c4c848ad94e0
Levent Yumerov <yumerov.levent@gmail.com>
2015-07-10 22:17:37 +0300

    install script modifications: 'touching' files before backuping it to prevend error messages

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+touch ~/.tmux.conf
 mv ~/.tmux.conf ~/.tmux.conf.old
 ln -s ~/dot-files/.tmux.conf ~/.tmux.conf
 
-mv ~/.bashr ~/bashr.old
+touch ~/.bashr
+mv ~/.bashr ~/.bashr.old
 ln -s ~/dot-files/.bashr ~/.bashr
